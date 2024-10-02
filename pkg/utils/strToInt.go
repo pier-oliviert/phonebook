3be70b81a6347a4b4f7a8b5112565e279f2a4cbb
Ainsey11 <ainsey11@gmail.com>
2024-10-02 18:46:48 +0100

    Making this part of the utils package as we seem to use it in a few places

---
+++ pkg/utils/strToInt.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"strconv"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+)
+
+// Helper function to convert string to *int32
+func ToInt32Ptr(s string) *int32 {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return nil
+	}
+	return to.Ptr(int32(i))
+}
