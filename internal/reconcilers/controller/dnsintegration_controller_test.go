014e4eb31e7c8edb96b483c784763960bed1100f
Pier-Olivier Thibault <23230+pier-oliviert@users.noreply.github.com>
2024-10-14 07:41:13 -0400

    Remove auto-generated TODO comments

--- internal/reconcilers/controller/dnsintegration_controller_test.go
+++ internal/reconcilers/controller/dnsintegration_controller_test.go
@@ -38,7 +38,7 @@ var _ = Describe("DNSIntegration Controller", func() {
 
 		typeNamespacedName := types.NamespacedName{
 			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+			Namespace: "default",
 		}
 		dnsprovider := &sequenceriov1alpha1.DNSIntegration{}
 
@@ -63,7 +63,6 @@ var _ = Describe("DNSIntegration Controller", func() {
 		})
 
 		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
 			resource := &sequenceriov1alpha1.DNSIntegration{}
 			err := k8sClient.Get(ctx, typeNamespacedName, resource)
 			Expect(err).NotTo(HaveOccurred())
@@ -82,8 +81,6 @@ var _ = Describe("DNSIntegration Controller", func() {
 				NamespacedName: typeNamespacedName,
 			})
 			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
 		})
 	})
 })
